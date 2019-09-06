5e9cbae094c6426fa0a98fdc419af985013195bd
momongaclub <threeforest8@gmail.com>
2019-09-06 13:21:30 +0900

    fix much funciton

--- rhyme_maker/Dictionary.py
+++ rhyme_maker/Dictionary.py
@@ -6,14 +6,14 @@ import argparse
 
 YOMI = 10
 
+
 class Dictionary():
-    
+
     def __init__(self):
         self.dictionary = {}
         self.word = ''
         self.yomi = ''
         self.vowel = ''
-
 
     def load_dictionary(self, fname):
         # csv形式,表層形,品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用型,活用形,原形,読み,発音
@@ -25,26 +25,27 @@ class Dictionary():
                 info = line[1:]
                 self.dictionary[word] = info
 
-
     def search_word_yomi(self):
         info = self.dictionary[self.word]
         self.yomi = info[YOMI]
 
-
     def word2vowel(self):
         vowel = ''
         patterns = []
-        a_pattern = re.compile("[アカサタナハマヤラワガザダバ]")
-        i_pattern = re.compile("[イキシチ二ヒミリギジヅビ]")
-        u_pattern = re.compile("[ウクスツヌフムユルグズヅブ]")
-        e_pattern = re.compile("[エケセテネへメレゲゼデベ]")
-        o_pattern = re.compile("[オコソトノホモヨロヲゴゾドボ]")
+        # ッ,ィ
+        a_pattern = re.compile("[アカサタナハマヤラワガザダバパャ]")
+        i_pattern = re.compile("[イキシチニヒミリギジヅビピ]")
+        u_pattern = re.compile("[ウクスツヌフムユルグズヅブプュ]")
+        e_pattern = re.compile("[エケセテネヘメレゲゼデベペ]")
+        o_pattern = re.compile("[オコソトノホモヨロヲゴゾドボポョ]")
+        n_pattern = re.compile("[ン]")
         patterns.append(a_pattern)
         patterns.append(i_pattern)
         patterns.append(u_pattern)
         patterns.append(e_pattern)
         patterns.append(o_pattern)
-        v_list = ['a','i','u','e','o']
+        patterns.append(n_pattern)
+        v_list = ['a', 'i', 'u', 'e', 'o', 'n']
 
         for w in self.yomi:
             cnt = 0
@@ -53,12 +54,12 @@ class Dictionary():
                     self.vowel = self.vowel + v_list[cnt]
                 cnt += 1
 
-
     def search_similar_vowels(self):
         for word, info in self.dictionary.items():
             vowel = info[-1]
             if vowel == self.vowel:
                 print(word, vowel)
+
 
 def main():
     dictionary = Dictionary()
--- rhyme_maker/word2VC.py
+++
@@ -1,2 +0,0 @@
-def word2VC():
-    return 0
