af8a3eb5a89d47b49b93ad08562e33f0952b61bb
momongaclub <threeforest8@gmail.com>
2019-09-12 16:42:13 +0900

    fix some function

--- rhyme_maker/Dictionary.py
+++ rhyme_maker/Dictionary.py
@@ -3,73 +3,72 @@ import sys
 import re
 import codecs
 import argparse
+import pykakasi
 
-YOMI = 10
+import Corpus
 
 
-class Dictionary():
+SPACE = ' '
+WORD = 0
+VOWEL = 1
+
+
+class Dictionary(Corpus.Corpus):
 
     def __init__(self):
-        self.dictionary = {}
-        self.word = ''
-        self.yomi = ''
-        self.vowel = ''
+        Corpus.Corpus.__init__(self)
+        self.words2vowels = {}
 
-    def load_dictionary(self, fname):
-        # csv形式,表層形,品詞,品詞細分類1,品詞細分類2,品詞細分類3,活用型,活用形,原形,読み,発音
-        with codecs.open(fname, 'r', 'euc_jp', 'ignore') as fp:
-            for line in fp:
-                line = line.rstrip('\n')
-                line = line.split(',')
-                word = str(line[0])
-                info = line[1:]
-                self.dictionary[word] = info
+    def load_vowel_dictionary(self, fname):
+        with codecs.open(fname, 'r', 'utf-8', 'ignore') as fp:
+            for word_and_vowel in fp:
+                word_and_vowel = word_and_vowel.rstrip('\n')
+                word_and_vowel = word_and_vowel.split(' ')
+                word = word_and_vowel[WORD]
+                vowel = word_and_vowel[VOWEL]
+                self.words2vowels[word] = vowel
 
-    def search_word_yomi(self):
-        info = self.dictionary[self.word]
-        self.yomi = info[YOMI]
+    def make_vowel_dict(self, fname):
 
-    def word2vowel(self):
-        vowel = ''
-        patterns = []
-        # ッ,ィ
-        a_pattern = re.compile("[アカサタナハマヤラワガザダバパャ]")
-        i_pattern = re.compile("[イキシチニヒミリギジヅビピ]")
-        u_pattern = re.compile("[ウクスツヌフムユルグズヅブプュ]")
-        e_pattern = re.compile("[エケセテネヘメレゲゼデベペ]")
-        o_pattern = re.compile("[オコソトノホモヨロヲゴゾドボポョ]")
-        n_pattern = re.compile("[ン]")
-        patterns.append(a_pattern)
-        patterns.append(i_pattern)
-        patterns.append(u_pattern)
-        patterns.append(e_pattern)
-        patterns.append(o_pattern)
-        patterns.append(n_pattern)
-        v_list = ['a', 'i', 'u', 'e', 'o', 'n']
+        def yomi2vowel(yomi):
+            vowel = ''
+            v_list = ['a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O', 'n']
+            for w in yomi:
+                if w in v_list:
+                    vowel = vowel + w
+            return vowel
 
-        for w in self.yomi:
-            cnt = 0
-            for pattern in patterns:
-                if pattern.match(w):
-                    self.vowel = self.vowel + v_list[cnt]
-                cnt += 1
+        def convert_vowel():
+            kakasi = pykakasi.kakasi()
+            kakasi.setMode("H", "a")
+            kakasi.setMode("K", "a")
+            kakasi.setMode("J", "a")
+            kakasi.setMode("r", "Hepburn")
+            kakasi.setMode("s", True)
+            kakasi.setMode("C", True)
+            conv = kakasi.getConverter()
+            for word in self.words:
+                yomi = conv.do(word)
+                yomi = str(yomi)
+                vowel = yomi2vowel(yomi)
+                self.words2vowels[word] = vowel
 
-    def search_similar_vowels(self):
-        for word, info in self.dictionary.items():
-            vowel = info[-1]
-            if vowel == self.vowel:
-                print(word, vowel)
+        convert_vowel()
+        with open(fname, 'w', encoding='utf-8') as fp:
+            for word, vowel in self.words2vowels.items():
+                fp.write(word + SPACE + vowel)
+                fp.write('\n')
 
 
 def main():
+    corpus_fname = sys.argv[1]
+    output_vowel_dict_fname = sys.argv[2]
+    input_vowel_dict_fname = sys.argv[3]
     dictionary = Dictionary()
-    fname = sys.argv[1]
-    word = sys.argv[2]
-    dictionary.load_dictionary(fname)
-    dictionary.word = word
-    dictionary.search_word_yomi()
-    dictionary.word2vowel()
-    dictionary.search_similar_vowels()
+    dictionary.load_sentences(corpus_fname)
+    dictionary.extract_words()
+    dictionary.make_vowel_dict(output_vowel_dict_fname)
+    dictionary.load_vowel_dictionary(input_vowel_dict_fname)
 
 
 if __name__ == '__main__':
