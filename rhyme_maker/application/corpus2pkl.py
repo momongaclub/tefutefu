f95f78743c4c3857780fccbdd396f1ca9d7489f8
momongaclub <threeforest8@gmail.com>
2020-06-22 13:50:32 +0900

    fitst commit

---
+++ rhyme_maker/application/corpus2pkl.py
@@ -0,0 +1,23 @@
+import sys
+import argparse
+from Class import Corpus
+
+
+def parser():
+    parser = argparse.ArgumentParser(description='Process corpus.')
+    parser.add_argument('corpus', help='corpus_data')
+    parser.add_argument('save_pkl', help='save_pkl_file')
+    args = parser.parse_args()
+    return args
+
+
+
+def main():
+    args = parser()
+    corpus = Corpus.Corpus()
+    corpus.load_corpus(args.corpus, f_type='binary')
+    corpus.save_pkl(args.save_pkl)
+
+
+if __name__ == '__main__':
+    main()
