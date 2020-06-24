243c855510eb745200e80e4e596da596ad81ed75
momongaclub <threeforest8@gmail.com>
2020-06-25 07:45:28 +0900

    add cosine_similarity_function

--- rhyme_maker/application/make_rhyme.py
+++ rhyme_maker/application/make_rhyme.py
@@ -3,6 +3,7 @@ from .Class import Vowel
 from pykakasi import kakasi
 import argparse
 import sys
+import gensim
 
 
 def parser():
@@ -15,15 +16,30 @@ def parser():
 def search_rhyme(query_word, corpus, vowel):
     rhymes = ''
     query_vowel = vowel.word2vowel(query_word)
-    print('query_word:', query_word, 'query_vowel:', query_vowel)
     # TODO word が [word]形式とword形式になっているので変換処理を書く
+    results = []
+    cnt = 0
     for word in corpus.word2vec.vocab:
         word_vowel = vowel.word2vowel(word)
         if query_vowel == word_vowel:
-            print('word:', word, 'vowel:', word_vowel)
+            # if query_wordがcorpus内にあれば
+            result = cosine_similarity(corpus, query_word, word)
+            result.append(word_vowel)
+            results.append(result)
             rhymes = rhymes + word + '(' + word_vowel + ')' + ','
-    return rhymes
+        cnt += 1
+        if cnt == 60000:
+            break
+    results = sorted(results, key=lambda x:x[1], reverse=True)
+    print(results)
+    return results
+    # return rhymes
 
+def cosine_similarity(corpus, query_word, target_word):
+    # model.most_similar(positive=['姪', '男性'], negative=['女性'])
+    cos_sim = corpus.word2vec.similarity(query_word, target_word)
+    result = [target_word, cos_sim]
+    return result
 
 def get_query_word(query_word):
     return query_word
@@ -34,8 +50,8 @@ def main(request):
     query_word = request.POST['input_text']
     vowel = Vowel.Vowel()
     corpus = Corpus.Corpus()
-    corpus.load_corpus('/home/mibayashi/programs/tefutefu/rhyme_maker/application/data/tohoku_wiki_embeddings/entity_vector.model.sample',
-                       f_type='txt')
+    # corpus.load_corpus('/home/mibayashi/programs/tefutefu/rhyme_maker/application/data/tohoku_wiki_embeddings/entity_vector.model.sample', f_type='txt')
+    corpus.load_corpus('/home/mibayashi/programs/tefutefu/rhyme_maker/application/data/tohoku_wiki_embeddings/entity_vector.model.pkl', f_type='pkl')
     rhymes = search_rhyme(query_word, corpus, vowel)
     return rhymes
 
